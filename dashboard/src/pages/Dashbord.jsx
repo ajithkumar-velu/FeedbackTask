f290760e9186eb12aa00d12b35f26a4147fc94e4
ajithkumar-velu <ajithajk46@gmail.com>
2024-11-10 11:08:07 +0530

    response2 update commit

--- dashboard/src/pages/Dashbord.jsx
+++ dashboard/src/pages/Dashbord.jsx
@@ -41,7 +41,7 @@ const Dashbord = () => {
     return latestFeedbackData[0] && (
         <div className="px-[4%] w-full bg-slate-100 pt-10" >
             <p className='md:text-4xl text-2xl font-bold  mb-12' >Feedback Dashbord</p>
-            <div /*className='flex items-center justify-center flex-col mb-10 2xl:flex-row 2xl:gap-5'*/ className='w-full flex flex-col items-center gap-10 md:grid grid-cols-2 xl:grid-cols-4  w-full h-full md:mb-10' >
+            <div /*className='flex items-center justify-center flex-col mb-10 2xl:flex-row 2xl:gap-5'*/ className='w-full flex flex-col items-center gap-10 md:grid grid-cols-2 xl:grid-cols-4 h-full mb-10' >
 {/* 
                 <div className='flex flex-col lg:flex-row gap-5 2xl:gap-5' >
                 </div>
@@ -52,9 +52,9 @@ const Dashbord = () => {
                     <p><TotalFeedback count={countOfSeller} userType={"sellers"} image={images.seller_icon} /></p>
                     <p><TotalFeedback count={countOfCustomer} userType={'customers'} image={images.customers_icon} /></p>
             </div>
-            <div className='xl:flex justify-evenly' >
+            <div className='xl:flex gap-10 justify-evenly' >
 
-                <div className='xl:w-[70%] bg-white p-10 rounded-2xl' >
+                <div className='xl:w-[70%] mb-5 bg-white p-10 rounded-2xl' >
 
                     <Char />
                 </div>
